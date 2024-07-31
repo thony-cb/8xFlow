4c8a4f5787ef65ddd52d346855dd596f8af8b106
Diego Cabrera <diego.thonycb@gmail.com>
2024-07-31 00:00:09 -0400

    Tailwind speed run message

--- components/layout/Hero.js
+++ components/layout/Hero.js
@@ -6,9 +6,13 @@ export default function Hero() {
     <div className="flex flex-col justify-between gap-[1.5rem] items-center py-[8rem]">
       <p className="text-base text-green-100">Webflow Agency</p>
       <h1 className="text-display">Your Webflow partner</h1>
-      <p className="max-w-lg text-base text-center text-white-125 ">
+      <p className="max-w-lg text-base leading-snug text-center text-white-125 ">
         We design and develop handcrafted websites for amazing companies who
-        want to win the digital game.
+        want to win the digital game. <br /> [
+        <span className="text-xs italic">
+          Tailwind speed run re-build 45 mins
+        </span>
+        ]
       </p>
       <div className="flex w-fit">
         <Button>Let's get in touch</Button>
